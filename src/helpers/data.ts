fe669ff5b86cd3460573f88db43c6aa1e14f29f4
李登洲 <lidengzhou@idcmatic.com>
2019-09-22 10:45:19 +0800

    feat: parseHeaders

--- src/helpers/data.ts
+++ src/helpers/data.ts
@@ -4,4 +4,5 @@ export function transformRequest(data: any): any {
   if (isPlainObject(data)) {
     return JSON.stringify(data)
   }
+  return data
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,22 +1,35 @@
 export type Method =
   | 'get'
   | 'GET'
-  | 'post'
-  | 'POST'
   | 'delete'
   | 'DELETE'
-  | 'put'
-  | 'PUT'
   | 'head'
   | 'HEAD'
   | 'options'
   | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
   | 'patch'
   | 'PATCH'
 
 export interface AxiosRequestConfig {
   url: string
+  headers?: any
   method?: Method
   data?: any
   params?: any
+  responseType?: XMLHttpRequestResponseType
 }
+
+export interface AxiosResponse {
+  data: any
+  status: number
+  statusText: string
+  headers: any
+  config: AxiosRequestConfig
+  request: any
+}
+
+export interface AxiosPromise extends Promise<AxiosResponse> {}
